140a141b1e6de39726ae319e113a71d8b5e9af19
adidamdileep <31473824+adidamdileep@users.noreply.github.com>
2021-08-19 21:12:20 +0530

    Delete rms-api directory

--- rms-api/src/main/java/com/deloitte/rms/api/Interviewer.java
+++
@@ -1,6 +0,0 @@
-package com.deloitte.rms.api;
-
-public class Interviewer extends Person {
-	private static final long serialVersionUID = 1L;
-
-}
--- rms-api/src/main/java/com/deloitte/rms/api/Referer.java
+++
@@ -1,5 +0,0 @@
-package com.deloitte.rms.api;
-
-public class Referer extends Person {
-	private static final long serialVersionUID = 1L;
-}
