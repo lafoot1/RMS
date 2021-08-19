d82f84850dfce9a814e01f770a275ef44b970ef0
adidamdileep <31473824+adidamdileep@users.noreply.github.com>
2021-08-19 21:13:04 +0530

    Delete rms-ui directory

--- rms-ui/js/model/Candidate.js
+++
@@ -1,4 +0,0 @@
-var Candidate = Backbone.Model.extend({
-	//urlRoot: './js/data/1.json'
-	urlRoot: App.Context.getValue('CANDIDATE_EP')
-});
--- rms-ui/js/model/Referral.js
+++
@@ -1,4 +0,0 @@
-var Referral = Backbone.Model.extend({
-	//url: './js/data/1.json'
-	urlRoot: App.Context.getValue('REFERRAL_EP')
-});
